fc362821b35e9a508673ccdca23aea6e0c61490f
Sandra <sandra@burdy.io>
2021-09-15 00:14:08 +0200

    fix: useModelState delete, settings sections, field configurations

--- src/admin/features/settings/tabs/general.tab.tsx
+++ src/admin/features/settings/tabs/general.tab.tsx
@@ -1,90 +1,19 @@
-import React, { useCallback, useEffect } from 'react';
-import LoadingBar from '@admin/components/loading-bar';
-import { DefaultButton, PrimaryButton, Stack } from '@fluentui/react';
-import { useSettings } from '@admin/context/settings';
+import React, { useMemo } from 'react';
 import Heading from '@admin/components/heading';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { ControlledTextField } from '@admin/components/rhf-components';
-import StatusBar from '@admin/components/status-bar';
-import Validators from '@shared/validators';
-
-const schema = yup.object({
-  adminEmail: Validators.email().label('Admin Email'),
-});
+import Hooks from '@shared/features/hooks';
 
 const GeneralSettings = () => {
-  const { getMainSettings, updateMainSettings } = useSettings();
-  const { control, handleSubmit, reset, formState } = useForm({
-    resolver: yupResolver(schema),
-    defaultValues: {},
-  });
-
-  const submit = handleSubmit(updateMainSettings.execute);
-
-  useEffect(() => {
-    getMainSettings.execute();
-
-    return () => {
-      updateMainSettings.reset();
-      getMainSettings.reset();
-    };
+  const sections = useMemo<any[]>(() => {
+    return Hooks.applySyncFilters('admin/settings/sections', []);
   }, []);
-
-  useEffect(() => {
-    reset(getMainSettings.result);
-  }, [getMainSettings.result]);
-
-  const dismissStatus = useCallback(() => {
-    getMainSettings.execute().then(() => {
-      reset();
-      updateMainSettings.reset();
-    });
-  }, [reset, updateMainSettings]);
-
   return (
     <div>
       <Heading title="General Settings" noPadding>
-        Here you will find general settings about the website configuration.
+        {!(sections?.length > 0) && 'You do not have any general settings sections.'}
       </Heading>
-      <LoadingBar loading={getMainSettings.loading}>
-        <form onSubmit={submit}>
-          <StatusBar
-            controller={updateMainSettings}
-            onDismiss={dismissStatus}
-            successMessage="Successfully updated"
-          />
-          <Stack tokens={{ childrenGap: 20 }} horizontalAlign="start">
-            <Stack tokens={{ childrenGap: 10 }} horizontalAlign="start">
-              <ControlledTextField
-                control={control}
-                label="Admin Email"
-                name="adminEmail"
-                type="email"
-                data-cy="settings-adminEmail"
-                style={{ minWidth: 400 }}
-                required
-              />
-            </Stack>
-            <Stack horizontal tokens={{ childrenGap: 10 }}>
-              <PrimaryButton
-                type="submit"
-                data-cy="settings-submit"
-                disabled={updateMainSettings.loading || !formState.isDirty}
-              >
-                Save
-              </PrimaryButton>
-              <DefaultButton
-                onClick={() => reset()}
-                disabled={updateMainSettings.loading || !formState.isDirty}
-              >
-                Reset
-              </DefaultButton>
-            </Stack>
-          </Stack>
-        </form>
-      </LoadingBar>
+      {sections.map(({key, component}) => (
+        <section key={key}>{component}</section>
+      ))}
     </div>
   );
 };
