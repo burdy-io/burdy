3bf9c930133d8c829fcdfccbcd01070765340f72
Sandra <sandra@burdy.io>
2021-09-15 13:21:46 +0200

    fix: safari styles

--- src/admin/components/loading-bar.tsx
+++ src/admin/components/loading-bar.tsx
@@ -12,7 +12,10 @@ const useStyles = makeStyles({
     display: 'flex !important',
     justifyContent: 'center',
     alignItems: 'center',
-    inset: 0,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
 
--- src/admin/features/assets/components/assets-dropzone.tsx
+++ src/admin/features/assets/components/assets-dropzone.tsx
@@ -13,7 +13,10 @@ const useStyles = makeStyles((theme) => {
     zone: {
       position: 'absolute',
       zIndex: 999,
-      inset: 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
